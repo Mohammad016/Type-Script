1bc9afc1b28acdabfb5ec1adf19c628624ae1d5a
Mohammad Arbaaz <95332128+Mohammad016@users.noreply.github.com>
2025-11-30 10:02:40 +0530

    Delete src directory

--- src/models/todoModel.ts
+++
@@ -1,6 +0,0 @@
-//This file becomes your “temporary database”.
-//For now, we store todos in an array.
-//Later, we plug in MongoDB/Postgres — no code rewriting needed because everything stays modular.
-import { Todo } from "../types/todo";
-
-export let todos: Todo[] = [];
--- src/types/todo.ts
+++
@@ -1,7 +0,0 @@
-// This is your bluePrint
-// This acts like a “shape” of the data, telling TypeScript what a Todo looks like.
-export interface Todo {
-    id: String;
-    title: String;
-    completed: Boolean;
-}
